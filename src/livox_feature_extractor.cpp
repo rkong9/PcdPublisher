8e17c97acb72e4c3ffc621eb82998412a87cd8d2
Ryan-IJK <firstunion@tutanota.com>
2025-02-10 18:48:57 +0800

    [feat] add livox feature extractor, not compiling
    Signed-off-by: Ryan-IJK <firstunion@tutanota.com>

--- src/livox_feature_extractor.cpp
+++ src/livox_feature_extractor.cpp
@@ -1,92 +1,475 @@
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl_ros/point_cloud.h>
+#include <Eigen/Dense>
+#include <chrono>
+#include <condition_variable>
+#include <deque>
+#include <fstream>
+#include <map>
+#include <mutex>
+#include <pcl/common/angles.h>
+#include <pcl/common/transforms.h>
 #include <pcl/filters/crop_box.h>
 #include <pcl/filters/voxel_grid.h>
-#include <pcl/common/transforms.h>
-#include <pcl/common/angles.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
-#include <Eigen/Dense>
-#include <vector>
-#include <mutex>
-#include <deque>
-#include <condition_variable>
+#include <pcl_ros/point_cloud.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <thread>
-#include <map>
-#include <chrono>
-#include <fstream>
+#include <vector>
 
 std::mutex mDataMutex;
 std::condition_variable mDataCv;
 std::deque<sensor_msgs::PointCloud2Ptr> mqData;
 
-void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
-{
-    sensor_msgs::PointCloud2Ptr pMsg(new sensor_msgs::PointCloud2);
-    *pMsg = *msg;
-    ROS_INFO("Get pcd cloud seq: %d", pMsg->header.seq);
+void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr &msg) {
+  sensor_msgs::PointCloud2Ptr pMsg(new sensor_msgs::PointCloud2);
+  *pMsg = *msg;
+  ROS_INFO("Get pcd cloud seq: %d", pMsg->header.seq);
 
-    // 将 PointCloud2 消息转换为 PCL 点云
-    std::unique_lock<std::mutex> lock(mDataMutex);
-    mqData.push_back(pMsg);
-    if (mqData.size() > 30) {
-        mqData.pop_front();
-    }
-    mDataCv.notify_all();
+  // 将 PointCloud2 消息转换为 PCL 点云
+  std::unique_lock<std::mutex> lock(mDataMutex);
+  mqData.push_back(pMsg);
+  if (mqData.size() > 30) {
+    mqData.pop_front();
+  }
+  mDataCv.notify_all();
 }
 
 void processThread() {
-    while (ros::ok())
+  while (ros::ok()) {
+    sensor_msgs::PointCloud2Ptr pMsg(nullptr);
     {
-        sensor_msgs::PointCloud2Ptr pMsg(nullptr);
-        {
-            std::unique_lock<std::mutex> lock(mDataMutex);
-            if (mqData.size() == 0) {
-                if (mDataCv.wait_for(lock, std::chrono::milliseconds(500)) ==
-                    std::cv_status::timeout) {
-                    ROS_WARN("wait pcd cloud overtime...");
-                    continue;
-                }
-            }
-            pMsg = mqData.front();
-            mqData.pop_front();
+      std::unique_lock<std::mutex> lock(mDataMutex);
+      if (mqData.size() == 0) {
+        if (mDataCv.wait_for(lock, std::chrono::milliseconds(500)) ==
+            std::cv_status::timeout) {
+          ROS_WARN("wait pcd cloud overtime...");
+          continue;
         }
-
-        if (!pMsg) {
-            ROS_WARN("invalid point cloud");
-            continue;
-        }
-
-        pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudIn(new pcl::PointCloud<pcl::PointXYZI>());
-        // pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudTrans(new pcl::PointCloud<pcl::PointXYZI>());
-        pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudOut(new pcl::PointCloud<pcl::PointXYZI>());
-
-        pcl::fromROSMsg(*pMsg, *pCloudIn);
+      }
+      pMsg = mqData.front();
+      mqData.pop_front();
     }
+
+    if (!pMsg) {
+      ROS_WARN("invalid point cloud");
+      continue;
+    }
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudIn(
+        new pcl::PointCloud<pcl::PointXYZI>());
+    // pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudTrans(new
+    // pcl::PointCloud<pcl::PointXYZI>());
+    pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudOut(
+        new pcl::PointCloud<pcl::PointXYZI>());
+    pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudGoodPts(
+        new pcl::PointCloud<pcl::PointXYZI>());
+
+    pcl::fromROSMsg(*pMsg, *pCloudIn);
+    extract_good_points(pCloudIn, pCloudGoodPts);
+  }
+}
+
+void computeFeatures() {
+  // step0. 从第二个点开始遍历每个点直至倒数第二个点
+  // stpe1. 去除掉过近点和无效点后计算每个点的曲率(相邻点也不能是过近或者无效点)
+  // step2.
+  // 计算当前点到激光中心这个向量与当前点前后2个邻点组成的向量的夹角(入射角)
+  // step3. 忽略掉入射角过小的点(阈值为10)
+  // step4. 对于有效点如果曲率小于设定阈值则认为是平面
+  // step5.
+  // 如果曲率大于阈值并且前后点的距离差小于10%,则认为是角点(过滤掉遮挡的点)
+}
+
+void extract_good_points(pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudIn,
+                         pcl::PointCloud<pcl::PointXYZI>::Ptr &pCloudOut) {
+  float corner_cav_th(0.05);
+  float surface_cav_th(0.05);
+  float max_fov(70.4); // degrees
+}
+
+LivoxFeature::LivoxFeature() {
+  // 57.3 是角度与弧度的转换单位 70.4 max_fov
+  m_max_edge_polar_pos = std::pow(tan(66.6 / 57.3) * 1, 2);
+}
+
+LivoxFeature::~LivoxFeature() {}
+
+void LivoxFeature::add_mask_of_point(Pt_infos *pt_infos,
+                                     const E_point_type &pt_type,
+                                     int neighbor_count = 0) {
+
+  int idx = pt_infos->idx;
+  pt_infos->pt_type |= pt_type;
+
+  if (neighbor_count > 0) {
+    for (int i = -neighbor_count; i < neighbor_count; i++) {
+      idx = pt_infos->idx + i;
+
+      if (i != 0 && (idx >= 0) && (idx < (int)m_pts_info_vec.size())) {
+        m_pts_info_vec[idx].pt_type |= pt_type;
+      }
+    }
+  }
+}
+
+float dis2_xy(float x, float y) { return x * x + y * y; }
+
+float depth2_xyz(float x, float y, float z) { return x * x + y * y + z * z; }
+
+void LivoxFeature::set_intensity(
+    pcl::PointXYZI &pt, int p_idx,
+    const E_intensity_type &i_type = e_I_motion_blur) {
+  Pt_infos *pt_info = m_pts_info_vec[p_idx];
+  switch (i_type) {
+  case (e_I_raw):
+    pt.intensity = pt_info->raw_intensity;
+    break;
+  case (e_I_motion_blur):
+    pt.intensity = ((float)pt_info->idx) / (float)m_input_points_size;
+    assert(pt.intensity <= 1.0 && pt.intensity >= 0.0);
+    break;
+  case (e_I_motion_mix):
+    pt.intensity =
+        0.1 * ((float)pt_info->idx + 1) / (float)m_input_points_size +
+        (int)(pt_info->raw_intensity);
+    break;
+  case (e_I_scan_angle):
+    pt.intensity = pt_info->polar_angle;
+    break;
+  case (e_I_curvature):
+    pt.intensity = pt_info->curvature;
+    break;
+  case (e_I_view_angle):
+    pt.intensity = pt_info->view_angle;
+    break;
+  case (e_I_time_stamp):
+    pt.intensity = pt_info->time_stamp;
+  default:
+    pt.intensity = ((float)pt_info->idx + 1) / (float)m_input_points_size;
+  }
+  return;
+}
+
+std::vector<PCloudXYZIPtr>
+LivoxFeature::preprocess_pointinfo(PCloudXYZI &cloudIn) {
+  size_t pts_size = cloudIn.size();
+  m_pts_info_vec.clear();
+  m_pts_info_vec.resize(pts_size);
+  m_raw_pts_vec.resize(pts_size);
+
+  int last_split_idx(0);
+  int scan_idx(0);
+
+  std::vector<PCloudXYZIPtr> vScans;
+  size_t curr_scan_pts(0);
+  PCloudXYZIPtr curr_scan_clouds(new PCloudXYZI);
+  curr_scan_clous->points.resize(pts_size);
+
+  for (size_t idx = 0; idx < pts_size; idx++) {
+    m_raw_pts_vec[idx] = cloudIn.points[idx];
+    Pt_infos *pt_info = &m_pts_info_vec[idx];
+    pt_info->raw_intensity = cloudIn.points[idx].intensity;
+    pt_info->idx = idx;
+    // pt_info->time_stamp = m_current_time + ((float)idx) *
+    // m_time_internal_pts;
+
+    if (!std::isfinite(CloudIn.points[idx].x) ||
+        !std::isfinite(cloudIn.points[idx].y) ||
+        !std::isfinite(cloudIn.points[idx].z)) {
+      add_mask_of_point(pt_info, e_pt_nan);
+      continue;
+    }
+
+    if (cloudIn.points[idx].x == 0) {
+      if (idx == 0) {
+        std::cout << "First point should be normal!!!" << std::endl;
+        pt_info->pt_2d_img << 0.01, 0.01;
+        pt_info->polar_dis_sq2 = 0.0001;
+        add_mask_of_point(pt_info, e_pt_000);
+      } else {
+        pt_info->pt_2d_img = m_pts_info_vec[idx - 1].pt_2d_img;
+        pt_info->polar_dis_sq2 = m_pts_info_vec[idx - 1].polar_dis_sq2;
+        add_mask_of_point(pt_info, e_pt_000);
+        continue;
+      }
+    }
+
+    pt_info->depth_sq2 = depth2_xyz(
+        cloudIn.points[idx].x, cloudIn.points[idx].y, cloudIn.points[idx].z);
+
+    pt_info->pt_2d_img << cloudIn.points[idx].y / cloudIn.points[idx].x,
+        cloudIn.points[idx].z / cloudIn.points[idx].x;
+    pt_info->polar_dis_sq2 =
+        dis2_xy(pt_info->pt_2d_img(0), pt_info->pt_2d_img(1));
+
+    if (pt_info->depth_sq2 <
+        m_livox_min_allow_dis * m_livox_min_allow_dis) // to close
+    {
+      add_mask_of_point(pt_info, e_pt_too_near);
+    }
+
+    pt_info->sigma = pt_info->raw_intensity / pt_info->polar_dis_sq2;
+
+    if (pt_info->sigma < m_livox_min_sigma) {
+      add_mask_of_point(pt_info, e_pt_reflectivity_low);
+    }
+
+    if (pt_info->polar_dis_sq2 > m_max_edge_polar_pos) {
+      add_mask_of_point(pt_info, e_pt_circle_edge, 2);
+    }
+
+    // Split scans
+    if (idx >= 1) {
+      float dis_incre =
+          pt_info->polar_dis_sq2 - m_pts_info_vec[idx - 1].polar_dis_sq2;
+
+      if (dis_incre > 0) // far away from zero
+      {
+        pt_info->polar_direction = 1;
+      }
+
+      if (dis_incre < 0) // move toward zero
+      {
+        pt_info->polar_direction = -1;
+      }
+
+      if (pt_info->polar_direction * m_pts_info_vec[idx - 1].polar_direction <
+          0) {
+        if (idx - last_split_idx) > 50 )
+                {
+            last_split_idx = idx;
+            scan_idx++;
+            curr_scan_clouds->points.resize(curr_scan_pts);
+            vScans.push_back(curr_scan_clouds);
+
+            PCloudXYZIPtr temp(new PCloudXYZI);
+            temp->resize(pts_size);
+            curr_scan_clouds = temp;
+          }
+      }
+
+      pt_info->scan_idx = scan_idx;
+      curr_scan_pts++;
+
+      // // 经过边缘点并且距离上一个分割点之间的点数超过50个
+      // if ( pt_info->polar_direction == -1 && m_pts_info_vec[ idx - 1
+      // ].polar_direction == 1 )
+      // {
+      //     if (idx -  last_split_idx) > 50 )
+      //     {
+      //         split_idx.push_back( idx );
+      //         edge_idx.push_back( idx );
+      //         continue;
+      //     }
+      // }
+
+      // // 经过原点并且距离上一个分割点之间的点数超过50个
+      // if ( pt_info->polar_direction == 1 && m_pts_info_vec[ idx - 1
+      // ].polar_direction == -1 )
+      // {
+      //     if ( zero_idx.size() == 0 || ( idx - split_idx[ split_idx.size() -
+      //     1 ] ) > 50 )
+      //     {
+      //         split_idx.push_back( idx );
+
+      //         zero_idx.push_back( idx );
+      //         continue;
+      //     }
+      // }
+    }
+  }
+  return vScans;
+}
+
+void LivoxFeature::compute_features() {
+  unsigned int pts_size = m_raw_pts_vec.size();
+  size_t curvature_ssd_size = 2;
+  int critical_rm_point = e_pt_000 | e_pt_nan;
+  float neighbor_accumulate_xyz[3] = {0.0, 0.0, 0.0};
+
+  for (size_t idx = curvature_ssd_size; idx < pts_size - curvature_ssd_size;
+       idx++) {
+    if (m_pts_info_vec[idx].pt_type & critical_rm_point) {
+      continue;
+    }
+
+    /*********** Compute curvate ************/
+    neighbor_accumulate_xyz[0] = 0.0;
+    neighbor_accumulate_xyz[1] = 0.0;
+    neighbor_accumulate_xyz[2] = 0.0;
+
+    for (size_t i = 1; i <= curvature_ssd_size; i++) {
+      if ((m_pts_info_vec[idx + i].pt_type & e_pt_000) ||
+          (m_pts_info_vec[idx - i].pt_type & e_pt_000)) {
+        if (i == 1) {
+          m_pts_info_vec[idx].pt_label |= e_label_near_zero;
+        } else {
+          m_pts_info_vec[idx].pt_label = e_label_invalid;
+        }
+        break;
+      } else if ((m_pts_info_vec[idx + i].pt_type & e_pt_nan) ||
+                 (m_pts_info_vec[idx - i].pt_type & e_pt_nan)) {
+        if (i == 1) {
+          m_pts_info_vec[idx].pt_label |= e_label_near_nan;
+        } else {
+          m_pts_info_vec[idx].pt_label = e_label_invalid;
+        }
+        break;
+      } else {
+        neighbor_accumulate_xyz[0] +=
+            m_raw_pts_vec[idx + i].x + m_raw_pts_vec[idx - i].x;
+        neighbor_accumulate_xyz[1] +=
+            m_raw_pts_vec[idx + i].y + m_raw_pts_vec[idx - i].y;
+        neighbor_accumulate_xyz[2] +=
+            m_raw_pts_vec[idx + i].z + m_raw_pts_vec[idx - i].z;
+      }
+    }
+
+    if (m_pts_info_vec[idx].pt_label == e_label_invalid) {
+      continue;
+    }
+
+    neighbor_accumulate_xyz[0] -= curvature_ssd_size * 2 * m_raw_pts_vec[idx].x;
+    neighbor_accumulate_xyz[1] -= curvature_ssd_size * 2 * m_raw_pts_vec[idx].y;
+    neighbor_accumulate_xyz[2] -= curvature_ssd_size * 2 * m_raw_pts_vec[idx].z;
+    m_pts_info_vec[idx].curvature =
+        neighbor_accumulate_xyz[0] * neighbor_accumulate_xyz[0] +
+        neighbor_accumulate_xyz[1] * neighbor_accumulate_xyz[1] +
+        neighbor_accumulate_xyz[2] * neighbor_accumulate_xyz[2];
+
+    /*********** Compute plane angle ************/
+    Eigen::Matrix<float, 3, 1> vec_a(m_raw_pts_vec[idx].x, m_raw_pts_vec[idx].y,
+                                     m_raw_pts_vec[idx].z);
+    Eigen::Matrix<float, 3, 1> vec_b(
+        m_raw_pts_vec[idx + curvature_ssd_size].x -
+            m_raw_pts_vec[idx - curvature_ssd_size].x,
+        m_raw_pts_vec[idx + curvature_ssd_size].y -
+            m_raw_pts_vec[idx - curvature_ssd_size].y,
+        m_raw_pts_vec[idx + curvature_ssd_size].z -
+            m_raw_pts_vec[idx - curvature_ssd_size].z);
+    m_pts_info_vec[idx].view_angle =
+        Eigen_math::vector_angle(vec_a, vec_b, 1) * 57.3;
+
+    // printf( "Idx = %d, angle = %.2f\r\n", idx,  m_pts_info_vec[ idx
+    // ].view_angle );
+    if (m_pts_info_vec[idx].view_angle > minimum_view_angle) {
+
+      if (m_pts_info_vec[idx].curvature < thr_surface_curvature) {
+        m_pts_info_vec[idx].pt_label |= e_label_surface;
+      }
+
+      float sq2_diff = 0.1;
+
+      if (m_pts_info_vec[idx].curvature > thr_corner_curvature) {
+        if (m_pts_info_vec[idx].depth_sq2 <=
+                m_pts_info_vec[idx - curvature_ssd_size].depth_sq2 &&
+            m_pts_info_vec[idx].depth_sq2 <=
+                m_pts_info_vec[idx + curvature_ssd_size].depth_sq2) {
+          if (abs(m_pts_info_vec[idx].depth_sq2 -
+                  m_pts_info_vec[idx - curvature_ssd_size].depth_sq2) <
+                  sq2_diff * m_pts_info_vec[idx].depth_sq2 ||
+              abs(m_pts_info_vec[idx].depth_sq2 -
+                  m_pts_info_vec[idx + curvature_ssd_size].depth_sq2) <
+                  sq2_diff * m_pts_info_vec[idx].depth_sq2)
+            m_pts_info_vec[idx].pt_label |= e_label_corner;
+        }
+      }
+    }
+  }
+}
+
+std::vector<PCloudXYZIPtr> LivoxFeature::processCloud(PCloudXYZIPtr &pCloudIn) {
+  m_input_points_size = pCloudIn.points.size();
+
+  std::vector<PCloudXYZIPtr> vClouds =
+      preprocess_pointinfo(laserCloudIn, scan_id_index);
+  compute_features();
+
+  return vClouds;
+}
+
+void LivoxFeature::getFeatures(pcl::PointCloud<pcl::PointXYZI> &pc_corners,
+                               pcl::PointCloud<pcl::PointXYZI> &pc_surface,
+                               pcl::PointCloud<pcl::PointXYZI> &pc_full_res,
+                               size_t begIdx, size_t endIdx, int color_mode) {
+
+  int corner_num = 0;
+  int surface_num = 0;
+  int full_num = 0;
+
+  pc_corners.resize(m_pts_info_vec.size()); // 前后2次resize 效率会更高
+  pc_surface.resize(m_pts_info_vec.size());
+  pc_full_res.resize(m_pts_info_vec.size());
+
+  int pt_critical_rm_mask = e_pt_000 | e_pt_nan | e_pt_too_near;
+
+  for (size_t i = begIdx; i < endIdx; i++) {
+    if ((m_pts_info_vec[i].pt_type & pt_critical_rm_mask) == 0) {
+      if (m_pts_info_vec[i].pt_label & e_label_corner) {
+        if (m_pts_info_vec[i].pt_type != e_pt_normal)
+          continue;
+        if (m_pts_info_vec[i].depth_sq2 < m_feature_corner_depth_sq2_th) {
+          pc_corners.points[corner_num] = m_raw_pts_vec[i];
+          pc_corners.points[corner_num].intensity =
+              m_pts_info_vec[i].time_stamp;
+          if (color_mode) {
+            set_intensity(pc_corners.points[corner_num], i,
+                          static_cast<E_intensity_type>(color_mode));
+          }
+          corner_num++;
+        }
+      }
+      if (m_pts_info_vec[i].pt_label & e_label_surface) {
+        if (m_pts_info_vec[i].depth_sq2 < m_feature_surface_depth_sq2_th) {
+          pc_surface.points[surface_num] = m_raw_pts_vec[i];
+          pc_surface.points[surface_num].intensity =
+              float(m_pts_info_vec[i].time_stamp);
+          if (color_mode) {
+            set_intensity(pc_surface.points[surface_num], i,
+                          static_cast<E_intensity_type>(color_mode));
+          }
+          surface_num++;
+        }
+      }
+    }
+    pc_full_res.points[full_num] = m_raw_pts_vec[i];
+    pc_full_res.points[full_num].intensity = m_pts_info_vec[i].time_stamp;
+    if (color_mode) {
+      set_intensity(pc_full_res.points[full_num], i,
+                    static_cast<E_intensity_type>(color_mode));
+    }
+    full_num++;
+  }
+
+  // printf("Get_features , corner num = %d, suface num = %d, blur from
+  // %.2f~%.2f\r\n", corner_num, surface_num, minimum_blur, maximum_blur);
+  pc_corners.resize(corner_num);
+  pc_surface.resize(surface_num);
+  pc_full_res.resize(full_num);
 }
 
 int main(int argc, char **argv) {
-        // 初始化 ROS
-    ros::init(argc, argv, "livox_feature_extractor");
-    ros::NodeHandle nh;
+  // 初始化 ROS
+  ros::init(argc, argv, "livox_feature_extractor");
+  ros::NodeHandle nh;
 
-    std::string cloudSource;
+  std::string cloudSource;
 
-    // ros::NodeHandle nh_private("~");
-    // nh_private.getParam("pcd_source", cloudSource);
-    // mGridRes = nh_private.param("grid_res", 0.5);
-    // mHeightTh = nh_private.param("height_th", 0.5);
-    // mGetMinZFrames = nh_private.param("get_min_z_frames", 10);
-    // ROS_INFO("Get pcd cloud source msg: %s", cloudSource.c_str());
-    ros::Subscriber pc_sub = nh.subscribe(cloudSource, 10, pointCloudCallback);
+  // ros::NodeHandle nh_private("~");
+  // nh_private.getParam("pcd_source", cloudSource);
+  // mGridRes = nh_private.param("grid_res", 0.5);
+  // mHeightTh = nh_private.param("height_th", 0.5);
+  // mGetMinZFrames = nh_private.param("get_min_z_frames", 10);
+  // ROS_INFO("Get pcd cloud source msg: %s", cloudSource.c_str());
+  ros::Subscriber pc_sub = nh.subscribe(cloudSource, 10, pointCloudCallback);
 
-    std::thread process(processThread);
+  std::thread process(processThread);
 
-    ros::spin();
-    process.join();
+  ros::spin();
+  process.join();
 
-    return 0;
+  return 0;
 }
---
+++ src/livox_feature_extractor.hpp
@@ -0,0 +1,101 @@
+#ifndef _LIVOX_FEATURE_EXTRACTOR_HPP_
+#define _LIVOX_FEATURE_EXTRACTOR_HPP_
+#include <map>
+#include <pcl/common/angles.h>
+#include <pcl/common/transforms.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+#include <pcl_ros/point_cloud.h>
+#include <vector>
+
+typedef pcl::PointCloud<pcl::PointXYZI> PCloudXYZI;
+typedef pcl::PointCloud<pcl::PointXYZI>::Ptr PCloudXYZIPtr;
+
+class LivoxFeature {
+public:
+  enum E_point_type {
+    e_pt_normal = 0,                      // normal points
+    e_pt_000 = 0x0001 << 0,               // points [0,0,0]
+    e_pt_too_near = 0x0001 << 1,          // points in short range
+    e_pt_reflectivity_low = 0x0001 << 2,  // low reflectivity
+    e_pt_reflectivity_high = 0x0001 << 3, // high reflectivity
+    e_pt_circle_edge = 0x0001 << 4,       // points near the edge of circle
+    e_pt_nan = 0x0001 << 5,               // points with infinite value
+    e_pt_small_view_angle = 0x0001 << 6,  // points with large viewed angle
+  };
+
+  enum E_feature_type // if and only if normal point can be labeled
+  { e_label_invalid = -1,
+    e_label_unlabeled = 0,
+    e_label_corner = 0x0001 << 0,
+    e_label_surface = 0x0001 << 1,
+    e_label_near_nan = 0x0001 << 2,
+    e_label_near_zero = 0x0001 << 3,
+    e_label_hight_intensity = 0x0001 << 4,
+  };
+
+  // Encode point infos using points intensity, which is more convenient for
+  // debugging.
+  enum E_intensity_type {
+    e_I_raw = 0,
+    e_I_motion_blur,
+    e_I_motion_mix,
+    e_I_sigma,
+    e_I_scan_angle,
+    e_I_curvature,
+    e_I_view_angle,
+    e_I_time_stamp
+  };
+
+  struct Pt_infos {
+    int pt_type = e_pt_normal;
+    int pt_label = e_label_unlabeled;
+    int idx = 0;
+    int scan_idx = 0;
+    float raw_intensity = 0.f;
+    float time_stamp = 0.0;
+    float polar_angle = 0.f;
+    int polar_direction = 0;
+    float polar_dis_sq2 = 0.f;
+    float depth_sq2 = 0.f;
+    float curvature = 0.0;
+    float view_angle = 0.0;
+    float sigma = 0.0;
+    Eigen::Matrix<float, 2, 1> pt_2d_img; // project to X==1 plane
+  };
+
+  LivoxFeature();
+  ~LivoxFeature();
+  void getFeatures(pcl::PointCloud<pcl::PointXYZI> &pc_corners,
+                   pcl::PointCloud<pcl::PointXYZI> &pc_surface,
+                   pcl::PointCloud<pcl::PointXYZI> &pc_full_res, size_t begIdx,
+                   size_t endIdx, int color_mode);
+
+  std::vector<PCloudXYZIPtr> processCloud(PCloudXYZIPtr &pCloudIn);
+  void set_intensity(pcl::PointXYZI &pt, int p_idx,
+                     const E_intensity_type &i_type = e_I_motion_blur);
+
+private:
+  // void extract_good_points();
+  void add_mask_of_point(Pt_infos *pt_infos, const E_point_type &pt_type,
+                         int neighbor_count = 0);
+  std::vector<PCloudXYZIPtr> preprocess_pointinfo(PCloudXYZIPtr &pCloudIn);
+  void compute_features();
+
+private:
+  pcl::PointCloud<pcl::PointXYZI>::Ptr pCloudIn;
+  std::vector<Pt_infos> m_pts_info_vec;
+  std::vector<pcl::PointXYZI> m_raw_pts_vec;
+  size_t m_input_points_size;
+  float m_livox_min_allow_dis = 1.0;
+  float m_livox_min_sigma = 7e-3;
+  float m_max_edge_polar_pos;
+  float m_feature_surface_depth_sq2_th = 1000000.0;
+  float m_feature_corner_depth_sq2_th = 900.0;
+};
+
+#endif
